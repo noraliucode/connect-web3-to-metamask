ba16b795ee19f7d4c4657546a7afa1e8c1d9ebe8
Nora Liu <noraliu0830@gmail.com>
2021-06-16 22:48:10 +0800

    feat: add get_accounts/refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Web3 from 'web3'
 import logo from './logo.svg';
 import './App.css';
@@ -8,35 +8,45 @@ import './App.css';
 
 function App() {
   useEffect(()=>{
-    const ethEnabled = async () => {
-      // No more having to check window.web3 for its currentProvider, we can simply use window.ethereum as the provider itself
-      if (window.ethereum) {
-        // calls the pop-up UI dialogue that asks the user’s permission to connect the dApp to MetaMask.
-        await window.ethereum.send('eth_requestAccounts');
-        // create a window.web3 object with our own version of web3, using the window.ethereum object as the input provider
-        window.web3 = new Web3(window.ethereum);
-        return true;
-      }
-      return false;
-    }
-    ethEnabled()
+    loadWeb3()
+    loadBlockchainData()
   },[])
+
+  const [account, setAccount] = useState('')
+
+  const loadWeb3 = async() => {
+    if (window.ethereum) {
+      // calls the pop-up UI dialogue that asks the user’s permission to connect the dApp to MetaMask.
+      await window.ethereum.send('eth_requestAccounts');
+      // create a window.web3 object with our own version of web3, using the window.ethereum object as the input provider
+      window.web3 = new Web3(window.ethereum);
+      return true;
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      // DO NOTHING...
+    }
+  }
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    console.log('loadBlockchainData accounts', accounts)
+    setAccount(accounts[0])
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        account: {account}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={loadBlockchainData}>getAccounts</button>
+        
       </header>
     </div>
   );
